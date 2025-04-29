8c6753b4204e2fec8f026f7cb2e0fee2e5d098a9
Abigail <abigail.nikijlw@gmail.com>
2025-04-29 23:26:49 +0700

    done error handler

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,11 +1,16 @@
 function errorHandler(err, req, res, next) {
-    let status = 500;
-    let message = "Internal Server Error";
 
     if(err.name === "BadRequest") {
         return res.status(400).json({ message: err.message });
     }
 
-    if
+    if(err.name === "Unauthorized") {
+        return res.status(401).json({ message: err.message });
+    }
 
+    if(err.name === "NotFound") {
+        return res.status(404).json({ message: err.message });
+    }
+
+  return res.status(500).json({ message: "Internal Server Error" });
 }

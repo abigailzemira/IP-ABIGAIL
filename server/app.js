a8ecd432b4d40eeb29acad1e6b86ac3fa0afe286
Abigail <abigail.nikijlw@gmail.com>
2025-05-02 18:36:19 +0700

    change process_env

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 // express setup
-require('dotenv').config()
+if(process.env.NODE_ENV !== "development") {
+    require('dotenv').config()
+}
 const express = require('express');
 const cors = require('cors');
 const app = express();
